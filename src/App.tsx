719f596b0053b7c5d8cad0474d994cc221fdd980
ViktorF <vfidler111@gmail.com>
2026-04-07 16:06:50 +0300

    project init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <section>
+        <h1>Hi there, wip</h1>
+      </section>
+    </>
+  );
+}
+
+export default App;
